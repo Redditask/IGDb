ed3c7e35f95e9e98410b23e36c11251765954a6b
Redditask <artyomageyko@gmail.com>
2023-02-27 19:53:32 +0300

    search dropdown list styles changes, added useTransition hook

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,29 +1,44 @@
-import React, {useState} from "react";
+import React, {useTransition, useState} from "react";
 
 import {useGetSearchResultsQuery} from "../../API/rawgApi";
+
+import {ImSearch} from "react-icons/im";
 
 import styles from "./Search.module.scss";
 
 import Input from "../UI/Input/Input";
-import {ImSearch} from "react-icons/im";
 
 const Search: React.FC = () => {
+    const [isPending, startTransition] = useTransition();
     const [searchText, setSearchText] = useState<string>("");
 
-    //const {data, isLoading} = useGetSearchResultsQuery({searchText});
+    const {data, isLoading} = useGetSearchResultsQuery({searchText}, {skip: !searchText});
 
-    const searchHandler = (event: any) => setSearchText(event.target.value);
-
-    //console.log(data)
+    const searchHandler = (event: any) => {
+        startTransition(()=>{
+            setSearchText(event.target.value);
+        });
+    };
 
     return (
         <div className={styles.search}>
-            <ImSearch/>
-            <Input
-                value={searchText}
-                onChange={searchHandler}
-                placeholder="Search"
-            />
+            <div className={styles.search__line}>
+                <ImSearch size={20}/>
+                <Input
+                    value={searchText}
+                    onChange={searchHandler}
+                    placeholder="Search"
+                />
+            </div>
+            {
+                data
+                    ?
+                    <div className={styles.search__content}>
+                        {data.results[0].name}
+                    </div>
+                    :
+                    <></>
+            }
         </div>
     );
 };
